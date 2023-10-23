4bc8b9070dbc7327771936f28fdb18c33b9434a9
omor777 <bengalifact26@gmail.com>
2023-10-23 12:25:51 +0600

    day 2 code work properly

--- src/app.js
+++ src/app.js
@@ -1,68 +1,84 @@
-window.onload = function () {
-  main();
-};
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+// Giveaway Ends On Sunday, 24 April 2020 8:00am
+const giveaway = document.getElementById("giveaway");
+const deadline = document.getElementById("deadline");
+const items = document.querySelectorAll(".deadline-format h1");
 
-function main() {
-  let date = new Date();
-  const timerContainer = document.getElementById("count-down");
-  const days = document.getElementById("day");
-  const hours = document.getElementById("hour");
-  const minute = document.getElementById("minute");
-  const second = document.getElementById("second");
-  let sec = parseInt(second.innerHTML);
-  let min = parseInt(minute.innerHTML);
-  let hour = parseInt(hours.innerHTML);
-  let day = parseInt(days.innerHTML);
+const tempDate = new Date();
+const tempYear = tempDate.getFullYear();
+const tempMonth = tempDate.getMonth();
+const tempDay = tempDate.getDate();
 
-  let clearId = setInterval(() => {
-    if (sec == 0) {
-      sec = 60;
-      min--;
-    }
-    sec--;
-    // second
-    if (sec < 10) {
-      second.innerHTML = "0" + sec;
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
+
+const year = futureDate.getFullYear();
+const month = months[futureDate.getMonth()];
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+const hour = futureDate.getHours();
+const minute = futureDate.getMinutes();
+
+giveaway.innerHTML = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hour}:${minute}am`;
+
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const currentTime = new Date().getTime();
+  const t = futureTime - currentTime;
+  /**
+   * 1s = 1000m
+   * 1m = 60s
+   * 1hr = 60m
+   * 1d = 24hr
+   */
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  // values in array
+  const values = [days, hours, minutes, seconds];
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
     } else {
-      second.innerHTML = sec;
+      return item;
     }
-    // minute
-    if (min == 0) {
-      min = 59;
-      hour--;
-    }
-    if (min < 10) {
-      minute.innerHTML = "0" + min;
-    } else {
-      minute.innerHTML = min;
-    }
-    // hour
-    if (hour == 0) {
-      hour = 23;
-      day--;
-    }
-    if (hour < 10) {
-      hours.innerHTML = "0" + hour;
-    } else {
-      hours.innerHTML = hour;
-    }
-    // day
-    if (day < 10) {
-      days.innerHTML = "0" + day;
-    } else {
-      days.innerHTML = day;
-    }
-    if (day == 0 && hour == 0 && min == 0 && sec == 0) {
-      clearInterval(clearId);
-    }
-  }, 1000);
+  }
+  items.forEach((item, index) => {
+    item.innerHTML = format(values[index]);
+  });
+  if (t < 0) {
+    clearInterval(id);
+    deadline.innerHTML = `<h4>Sorry, this giveaway has expired!</h4>`;
+  }
 }
-
-// function createElement(parent) {
-//   const div = document.createElement("div");
-//   const h1 = document.createElement("h1");
-//   const p = document.createElement("p");
-//   div.appendChild(h1);
-//   div.appendChild(p);
-//   parent.appendChild(div);
-// }
+// countdown
+const id = setInterval(getRemainingTime, 1000);
+getRemainingTime();
